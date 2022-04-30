205416b165f75220d860064db4ea9af4d19381d6
Manoj Funde <funde.manoj@gmail.com>
2022-04-30 17:18:07 +0530

    My third commit

---
+++ pattern.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -x
+
+N=5
+i=0
+j=0
+
+while [ $i -le `expr $N-1`]
+do
+   j=0
+   while [ $j -le `expr $N -1` ]
+   do
+     if [ `expr $N - 1` -le `expr $i + $j` ]
+     then
+	echo -ne "#"
+    else
+        echo -ne "  "
+    fi
+    j= `expr $j + 1`
+   done
+   echo
+   i=` expr $j + 1`
+done
